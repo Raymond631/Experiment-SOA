0e85c136a77d2d890b98e063ac4a006971833f50
mwh <1372942864@qq.com>
2023-11-27 10:58:38 +0800

    给借还模块添加swagger

--- exp-02/src/main/java/com/example/exp02/controller/BorrowController.java
+++ exp-02/src/main/java/com/example/exp02/controller/BorrowController.java
@@ -3,6 +3,8 @@ package com.example.exp02.controller;
 import com.example.exp02.common.response.CommonResponse;
 import com.example.exp02.pojo.Borrow;
 import com.example.exp02.service.BorrowService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,47 +12,49 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@Api(tags = "借还模块")
 public class BorrowController {
 
     @Autowired
     private BorrowService borrowService;
 
+    @ApiOperation("添加借用信息")
     @PostMapping("/borrows")
     public CommonResponse addBorrowInfo(@RequestBody Borrow borrow) {
         borrowService.addBorrowInfo(borrow);
         return CommonResponse.success("添加借用信息成功", borrow, "/borrows/" + borrow.getId());
     }
-
+    @ApiOperation("删除借用信息")
     @DeleteMapping("/borrows/{id}")
     public CommonResponse deleteBorrowInfo(@PathVariable String id) {
         borrowService.deleteBorrowInfo(id);
         return CommonResponse.success("删除成功", null, "/borrows");
     }
-
+    @ApiOperation("获取所有借用信息")
     @GetMapping("/borrows")
     public CommonResponse getAllBorrowInfo() {
         List<Borrow> borrowList = borrowService.getAllBorrowInfo();
         return CommonResponse.success("获取所有借用信息成功", borrowList, null);
     }
-
+    @ApiOperation("根据id获取借用信息")
     @GetMapping("/borrows/{id}")
     public CommonResponse getBorrowInfoById(@PathVariable String id) {
         Borrow borrow = borrowService.getBorrowInfoById(id);
         return CommonResponse.success("获取借用信息成功", borrow, null);
     }
-
+    @ApiOperation("根据物品id获取借用信息")
     @GetMapping("/borrows/thing/{thingId}")
     public CommonResponse getBorrowInfoByThingId(@PathVariable("thingId") String thingId) {
         Borrow borrow = borrowService.getBorrowInfoByThingId(thingId);
         return CommonResponse.success("获取借用信息成功", borrow, null);
     }
-
+    @ApiOperation("根据人员id获取借用信息")
     @GetMapping("/borrows/person/{personId}")
     public CommonResponse getBorrowInfoByPersonId(@PathVariable("personId") String personId) {
         Borrow borrow = borrowService.getBorrowInfoByPersonId(personId);
         return CommonResponse.success("获取借用信息成功", borrow, null);
     }
-
+    @ApiOperation("更新借用信息")
     @PutMapping("/borrows/{id}")
     public CommonResponse updateBorrowInfo(
             @RequestBody Borrow borrow
@@ -58,7 +62,7 @@ public class BorrowController {
         borrowService.updateBorrowInfo(borrow);
         return CommonResponse.success("更新借用信息成功", borrow, null);
     }
-
+    @ApiOperation("更新借用返还时间")
     @PatchMapping("/borrows/{id}")
     public CommonResponse updateBorrowInfoByReturn(
             @PathVariable String id,
