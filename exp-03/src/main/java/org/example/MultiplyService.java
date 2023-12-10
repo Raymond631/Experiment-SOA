8df94d70267a2ccbc1a7d184ec674aeeb49aabd4
Raymond <raymondli631@qq.com>
2023-12-10 20:48:03 +0800

    实验三（个人实验）

---
+++ exp-03/src/main/java/org/example/MultiplyService.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+
+import java.util.logging.Logger;
+
+public class MultiplyService {
+    private final static Logger LOGGER = Logger.getLogger(AddService.class.getName());
+
+    public static void main(String[] args) {
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest") //流程运行地址
+                .asyncResponseTimeout(10000) //轮询间隔
+                .build();
+
+        // subscribe to an external task topic as specified in the process
+        client.subscribe("multiplyEvent").lockDuration(1000)
+                // the default lock duration is 20 seconds, but you can override this
+                .handler((externalTask, externalTaskService) -> {
+                    // Get a process variable
+                    Long t = (Long) externalTask.getVariable("t");
+                    Long c = (Long) externalTask.getVariable("c");
+                    VariableMap variableMap = Variables.createVariables().putValue("result", t * c);
+                    LOGGER.info("multiply " + t + " and " + c);
+                    // Complete the task
+                    externalTaskService.complete(externalTask, variableMap);
+                })
+                .open();
+    }
+}
